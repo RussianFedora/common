627cbe1c70fc7538b9e3951d24b17d5f70a300e7
Tigro <ashejn@gmail.com>
2013-04-09 15:44:59 +0400

    added condition to skip fetching sources if its exist

--- rfr_get_sources.sh
+++ rfr_get_sources.sh
@@ -8,8 +8,21 @@ fi
 if [ -f ./get_sources.sh ]; then
     sh ./get_sources.sh
 elif [ -x /usr/bin/spectool ]; then
-    /usr/bin/spectool -g *.spec
+    RUNSPECTOOL="no"
+    for i in `spectool -l -S *.spec | awk -F"[ /]" '{ print $NF }'`; do
+        if [ ! -f $i ]; then
+             echo NO $i found;
+             RUNSPECTOOL="yes"
+             break;
+        fi
+    done
+
+    if [ "$RUNSPECTOOL" == "yes" ]; then
+        /usr/bin/spectool -g *.spec
+    else
+        echo "All sources found"
+    fi
 else
-    echo "Cannot download source"
+    echo "Cannot download source. No get_source.sh nor spectool found."
     exit 1
 fi
